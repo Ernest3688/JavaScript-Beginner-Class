00d368fff636c4504d27596200dcef5ff5971936
Ernest3688 <88985578+Ernest3688@users.noreply.github.com>
2024-03-03 04:45:22 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+/*create a new github repo called "JavaScript Beginner Class"
+connect it with your local repository
+solve the problems below in a JavaScript file added.
+
+ create a variable named result and assign  the value true to it using the var keyword. */
+
+var result = "true";
+// declare a variable called userName and initialize it with your name using the let keyword
+
+let userName = "Ernest";
+// define a variable named favoriteNumber and assign it a numerical value representing your favorite Number
+let favoriteNumber = 8;
+// declare a variable greeting without assigning any value to it. 
+let greeting;
+// then, assign the string 'Hello world!' to the variable on the next 
+ greeting = "Hello world!";
+// create a constant variable named pi and assign the value 3.14 to it usin the const keyword.
+const pi = 3.14;
+// use the typeof operator to check the type the type of the variable age and assign the result to a variable named typeOfAge
+let typeofAge = typeof age;
+// declare two variables num1 and num2 in instanceof.
+let num1 = 2
+let num2 = 4
+// define a variable isRaining and assign it a boolean value representing whether it is currently raining or Notification.
+let sum = num1 + num2;
+let isRaining = false;
+// declare a varable course and initialize it with the string "JavaScript Basic" in a singleline.
+let course = "JavaScript Basics";
+// reassign the value of the variable count from 5 to 10 using the let keyword.
+let count = 5;
+// declare a variable called city and initialize it with the name of your favorite city using the const keyword.
+const city = "London";
+// define a variable named myArray and assign it an array containing three different types of elements (e.g, a Number, a String, and a boolean).
+let myArray = [6, "Almond", false];
+// declare a variable temperature without assigning any value to it.
+let myTemperature;
+// later, assign the value 25 to the variable.
+ myTemperature = 25;
+// create a variable named price and assign it a numerical Value. Then reassign the value to price by  doubling its current value using const keyword.
+let price = 80;
+price *= 2
+// use the console.log statement to print the message "Hello JavaScript!" to the console
+console.log("Hello, JavaScript!");
+// Declare Two variables firstName and lastName and concatenate them to create a variable called fullName. print the fullName to console.
+var firstName = "Ernesti";
+var lastName = "chigo";
+ fullName = firstName + " " + lastName;
+console.log(fullName);
+// Create a variable named isStudent and assign it a boolean value representing whether you are currently a student or not.
+let isStudent = true;
+// Declare a variable carBrand and initialize it with the string "Toyota". Later, update the value to "Honda".
+let carBrand = "Toyota";
+ carBrand = "Honda";
+// Declare a variable named counter using the let keyword and assign it an initial value of 1. Increase the value of the counter by 3.
+let counter = 1;
+ counter+= 3;
+
+// 4. Push your solutions to github (Check and ensure you do not have any error before pushing)
+// 5. ⁠Send in your github link before 12am Friday morning
